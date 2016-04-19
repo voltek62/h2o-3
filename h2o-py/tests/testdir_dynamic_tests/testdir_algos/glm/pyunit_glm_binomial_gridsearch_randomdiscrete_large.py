4779f07d11aba8cf035be2797fd855575bfe82d5
wendycwong <wendycwong@yahoo.com>
2016-04-19 10:29:25 -0700

    PUBDEV-1843: grid testing.  Subtask 9.  Increase max_runtime_secs upper limit.  It is randomly generated.

--- h2o-py/tests/testdir_dynamic_tests/testdir_algos/glm/pyunit_glm_binomial_gridsearch_randomdiscrete_large.py
+++ h2o-py/tests/testdir_dynamic_tests/testdir_algos/glm/pyunit_glm_binomial_gridsearch_randomdiscrete_large.py
@@ -97,7 +97,7 @@ class Test_glm_random_grid_search:
     max_model_number = 0    # maximum number of models specified to test for stopping conditions, generated later
     max_grid_runtime = 1          # maximum runtime value in seconds, 1 minute max
     allowed_scaled_overtime = 0.1   # used to set max_allowed_runtime as allowed_scaled_overtime * total model run time
-    allowed_scaled_time = 0.2       # scale back time
+    allowed_scaled_time = 1       # scale back time
     allowed_scaled_model_number = 1.5   # used to set max_model_number as
     # possible_number_models * allowed_scaled_model_number
     max_stopping_rounds = 10            # maximum stopping rounds allowed to be used for early stopping metric
